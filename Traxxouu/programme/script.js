506f187ea1a3fef0315e3048b711cb3fe2e81d53
Traxxouu <83843025+Traxxouu@users.noreply.github.com>
2024-08-03 15:59:20 +0200

    Add files via upload

---
+++ Traxxouu/programme/script.js
@@ -0,0 +1,15 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function () {
+    // Activer WOW.js pour les animations
+    new WOW().init();
+
+    // Smooth scrolling pour les liens de navigation
+    document.querySelectorAll('a.nav-link').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
